e6e7c07bc26cb650461a4747dad9d012d2ce41f9
JayPaulinCodes <jacobwpaulin@gmail.com>
2022-07-04 21:28:40 -0400

    Video Break 1:56:59

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ consteval int get_value(){
 }
 
 int main(){
-    constexpr int value = get_value();
-    std::cout << "value : " << value << std::endl;
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
